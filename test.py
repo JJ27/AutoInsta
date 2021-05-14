38421fbc2cc4661f8236f3eab7d560fbd56532ed
JoshJ <hellojosh04@gmail.com>
2021-05-14 16:26:32 -0400

    Comment Old Login Button

--- test.py
+++ test.py
@@ -7,15 +7,15 @@ browser.implicitly_wait(5)
 
 browser.get('https://www.instagram.com/')
 
-login_link = browser.find_element_by_xpath("//a[text()='Log in']")
-login_link.click()
+#login_link = browser.find_element_by_xpath("//a[text()='Log in']")
+#login_link.click()
 
 sleep(2)
 
 username_input = browser.find_element_by_css_selector("input[name='username']")
 password_input = browser.find_element_by_css_selector("input[name='password']")
 
-username_input.send_keys("<your username>")
+username_input.send_keys("inactiveoldfanacc")
 password_input.send_keys("<your password>")
 
 login_button = browser.find_element_by_xpath("//button[@type='submit']")
