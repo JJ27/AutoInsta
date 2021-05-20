54377fd00f5e962bb82d53bd9cdf4c198a6896e2
JoshJ <hellojosh04@gmail.com>
2021-05-19 20:08:12 -0400

    Test 100 unfollow

--- classes.py
+++ classes.py
@@ -1,5 +1,13 @@
 from instapy import InstaPy
+from time import sleep
+import random
 
 session = InstaPy(username="inactiveoldfanacc", password="tester").login()
 
-session.unfollow_users(amount=100, nonFollowers=True, style="RANDOM", unfollow_after=10, sleep_delay=655)
+for i in range(100):
+    current = random.randint(0,45)
+    session.unfollow_users(amount=1, nonFollowers=True, style="RANDOM", unfollow_after=0, sleep_delay=0)
+    print(current);
+    sleep(current)
+
+session.close()
